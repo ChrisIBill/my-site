9a4642698bed3cebce9900569f0c6b74e3260b5a
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-01-13 08:30:57 -0600

    Initial

---
+++ src/Pages/Home.js
@@ -0,0 +1,34 @@
+import "./Home.scss";
+import { Outlet, Link } from "react-router-dom";
+import { useState } from "react";
+
+export default function Home() {
+  const [open, setOpen] = useState(true);
+  const menuButtonClick = () => {
+    setOpen(!open);
+  };
+  return (
+    <div id="Page">
+      <div id="header">Header</div>
+      <div id={open ? "sidebar" : "sidebar:closed"}>
+        <nav>
+          <button id="menuButton" onClick={menuButtonClick}></button>
+          <ul>
+            <li>
+              <Link to={`/About`}>About Me</Link>
+            </li>
+            <li>
+              <Link to={`/Calculator`}>Calulator</Link>
+            </li>
+            <li>
+              <Link to={`/Settings`}>Settings</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="Content">
+        <Outlet />
+      </div>
+    </div>
+  );
+}

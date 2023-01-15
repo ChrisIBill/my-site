02524b3d93a490915aad218219352f063e66643d
ChrisIBill <chris@Chris-Spectre>
2023-01-15 00:18:35 -0600

    Calculator buttons

--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -1,3 +1,21 @@
+import "./calculator.css"
+import { calcButtons } from "../lib/calcButtons";
+
+const CalcButtons = () => {
+    return (
+        <div className="calcContent">
+            {calcButtons.map((button) => {
+        return (
+          <div key={button.id} className="calcButtonContainer">
+            {/* {item.icon} */}
+            <button className="calcButton">{button.text}</button>
+          </div>
+        );
+      })}
+        </div>
+    )
+}
+
 export default function Calculator() {
-    return <div>Calculator</div>;
+    return <CalcButtons />;
 }
---
+++ src/lib/calcButtons.tsx
@@ -0,0 +1,66 @@
+export const calcButtons = [
+    {
+        id: 0,
+        text: '1',
+    },
+    {
+        id: 1,
+        text: '2',
+    },
+    {
+        id: 2,
+        text: '3',
+    },
+    {
+        id: 3,
+        text: '+',
+    },
+    {
+        id: 4,
+        text: '4',
+    },
+    {
+        id: 5,
+        text: '5',
+    },
+    {
+        id: 6,
+        text: '6',
+    },
+    {
+        id: 7,
+        text: '-',
+    },
+    {
+        id: 8,
+        text: '7',
+    },
+    {
+        id: 9,
+        text: '8',
+    },
+    {
+        id: 10,
+        text: '9',
+    },
+    {
+        id: 11,
+        text: '/',
+    },
+    {
+        id: 12,
+        text: '(',
+    },
+    {
+        id: 13,
+        text: ')',
+    },
+    {
+        id: 14,
+        text: '0',
+    },
+    {
+        id: 15,
+        text: '*',
+    },
+]

ee8f1ec132fb82928e63c4b5a6621ddd1d215421
ChrisIBill <chris@Chris-Spectre>
2023-01-15 00:18:35 -0600

    Calculator buttons

--- src/Components/sidebar.tsx
+++ src/Components/sidebar.tsx
@@ -6,16 +6,19 @@ import "./sidenav.css";
 export default function Sidebar() {
   const pages = Router.routes;
   return (
-    <div>
+    <div id="sidebar">
       <button className="menuButton"></button>
-      {pages.map((item) => {
-        return (
-          <div key={item.id} className="sideitem">
-            {/* {item.icon} */}
-            <Link to={item.id}>{item.id}</Link>
-          </div>
-        );
-      })}
+      <ul>
+        <li>
+          <Link to={`/About`}>About Me</Link>
+        </li>
+        <li>
+          <Link to={`/Calculator`}>Calulator</Link>
+        </li>
+        <li>
+          <Link to={`/Settings`}>Settings</Link>
+        </li>
+      </ul>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
 import './index.scss';
-import { Router }  from "./lib/pagesData"
+import { Router } from "./lib/pagesData"
 import Root from "./Pages/Root";
 import ErrorPage from "./Pages/ErrorPage"
 import Contact from "./Pages/Contact";
@@ -26,11 +26,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-   <RouterProvider router={Router} />
+    <RouterProvider router={Router} />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
